edad7bcf8b5a24a5f6744693a93e3b7eb1ebbfde
Nico Korthout <nicokorthout@gmail.com>
2021-07-03 14:17:37 +0200

    test(utils): avoid compose body regressions
    Adds tests for templates containing none or any of the available
    placeholders and verifies that they are correctly used to replace values
    in the way it works currently.
    
    Note, that currently it fails to replace multiple of the same
    placeholders.

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,5 +1,5 @@
 import dedent from "dedent";
-import { getMentionedIssueRefs } from "../utils";
+import { getMentionedIssueRefs, composeBody } from "../utils";
 
 describe("get mentioned issues", () => {
   describe("returns an empty list", () => {
@@ -128,8 +128,87 @@ describe("get mentioned issues", () => {
       ]);
     });
   });
+});
 
-  // todo deal with urls to unrelated repos
+describe("compose body", () => {
+  const main_default = {
+    number: 123,
+    body: "foo-body",
+  };
+  const target = "foo-target";
+
+  describe("returns same value as provided template", () => {
+    it("for an empty template", () => {
+      expect(composeBody("", main_default, target)).toEqual("");
+    });
+
+    it("for a template without placeholders", () => {
+      const template = text({});
+      expect(composeBody(template, main_default, target)).toEqual(template);
+    });
+
+    it("for a template with unknown placeholders", () => {
+      const template = text({
+        start: "${abc}",
+        middle: "${def}",
+        end: "${ghi}",
+        part: "${jkl}",
+      });
+      expect(composeBody(template, main_default, target)).toEqual(template);
+    });
+  });
+
+  describe("returns evaluated templated", () => {
+    it("for a template with target_branch placeholder", () => {
+      const template = "Backport of some-title to `${target_branch}`";
+      expect(composeBody(template, main_default, target)).toEqual(
+        "Backport of some-title to `foo-target`"
+      );
+    });
+
+    it("for a template with pull_number placeholder", () => {
+      const template = "Backport of #${pull_number} to some-target";
+      expect(composeBody(template, main_default, target)).toEqual(
+        "Backport of #123 to some-target"
+      );
+    });
+
+    describe("for a template with issue_refs placeholder", () => {
+      const template = "Backport that refers to: ${issue_refs}";
+
+      it("and body has no referred issues", () => {
+        expect(composeBody(template, main_default, target)).toEqual(
+          "Backport that refers to: "
+        );
+      });
+
+      it("and body has a referred issue", () => {
+        expect(
+          composeBody(
+            template,
+            {
+              ...main_default,
+              body: "Body mentions #123 and that's it.",
+            },
+            target
+          )
+        ).toEqual("Backport that refers to: #123");
+      });
+
+      it("and body has some referred issues", () => {
+        expect(
+          composeBody(
+            template,
+            {
+              ...main_default,
+              body: "This body refers to #123 and foo/bar#456",
+            },
+            target
+          )
+        ).toEqual("Backport that refers to: #123 foo/bar#456");
+      });
+    });
+  });
 });
 
 function text({
