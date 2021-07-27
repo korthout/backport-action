b92ae1c339f3520eb2a41200a730bda99c976fb8
Nico Korthout <nico.korthout@camunda.com>
2021-07-27 17:23:50 +0200

    refactor(exec): remove unused callBackportScript function
    Now that backport.sh is no longer used directly, we can remove the
    function

--- src/exec.ts
+++ src/exec.ts
@@ -20,26 +20,6 @@ export async function performBackport(
   );
 }
 
-export async function callBackportScript(
-  pwd: string,
-  headref: string,
-  baseref: string,
-  target: string,
-  branchname: string,
-  version: string
-): Promise<number> {
-  return exec(
-    `/home/runner/work/_actions/zeebe-io/backport-action/${version}/backport.sh`,
-    [pwd, headref, baseref, target, branchname],
-    {
-      listeners: {
-        stdout: (data) => console.log(data.toString()),
-      },
-      ignoreReturnCode: true,
-    }
-  );
-}
-
 export async function call(command: string): Promise<number> {
   return exec(command);
 }
