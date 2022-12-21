cc2d5927e5c1e4c60c27b541728f1559f4d5b8da
Nico Korthout <nico.korthout@camunda.com>
2022-12-21 16:50:05 +0100

    dist: build new version

--- dist/index.js
+++ dist/index.js
@@ -233,7 +233,7 @@ class Backport {
                 git cherry-pick -x $ancref..${headref}
                 \`\`\``
             : (0, dedent_1.default) `Note that rebase and squash merges are not supported at this time.
-                For more information see https://github.com/zeebe-io/backport-action/issues/46.`;
+                For more information see https://github.com/korthout/backport-action/issues/46.`;
         return (0, dedent_1.default) `Backport failed for \`${target}\`, ${reason}.
 
                   Please cherry-pick the changes locally.
