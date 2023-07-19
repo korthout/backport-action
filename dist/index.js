c08cdc806a91eb7cd76b658064b79c624aa619d1
Nico Korthout <korthout@users.noreply.github.com>
2023-07-19 19:37:15 +0000

    dist: build new artifacts
    Signed-off-by: Nico Korthout <korthout@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -5608,43 +5608,38 @@ module.exports = resolveCommand;
 /***/ }),
 
 /***/ 5281:
-/***/ ((module) => {
+/***/ ((module, exports) => {
 
 "use strict";
 
 
-function dedent(strings) {
-
-  var raw = void 0;
-  if (typeof strings === "string") {
-    // dedent can be used as a plain function
-    raw = [strings];
-  } else {
-    raw = strings.raw;
-  }
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports["default"] = dedent;
+function dedent(strings, ...values) {
+  const raw = typeof strings === "string" ? [strings] : strings.raw;
 
   // first, perform interpolation
-  var result = "";
-  for (var i = 0; i < raw.length; i++) {
-    result += raw[i].
+  let result = "";
+  for (let i = 0; i < raw.length; i++) {
+    result += raw[i]
     // join lines when there is a suppressed newline
-    replace(/\\\n[ \t]*/g, "").
-
+    .replace(/\\\n[ \t]*/g, "")
     // handle escaped backticks
-    replace(/\\`/g, "`");
-
-    if (i < (arguments.length <= 1 ? 0 : arguments.length - 1)) {
-      result += arguments.length <= i + 1 ? undefined : arguments[i + 1];
+    .replace(/\\`/g, "`");
+    if (i < values.length) {
+      result += values[i];
     }
   }
 
   // now strip indentation
-  var lines = result.split("\n");
-  var mindent = null;
-  lines.forEach(function (l) {
-    var m = l.match(/^(\s+)\S+/);
+  const lines = result.split("\n");
+  let mindent = null;
+  for (const l of lines) {
+    const m = l.match(/^(\s+)\S+/);
     if (m) {
-      var indent = m[1].length;
+      const indent = m[1].length;
       if (!mindent) {
         // this is the first indented line
         mindent = indent;
@@ -5652,24 +5647,22 @@ function dedent(strings) {
         mindent = Math.min(mindent, indent);
       }
     }
-  });
-
-  if (mindent !== null) {
-    result = lines.map(function (l) {
-      return l[0] === " " ? l.slice(mindent) : l;
-    }).join("\n");
   }
-
+  if (mindent !== null) {
+    const m = mindent; // appease TypeScript
+    result = lines
+    // https://github.com/typescript-eslint/typescript-eslint/issues/7140
+    // eslint-disable-next-line @typescript-eslint/prefer-string-starts-ends-with
+    .map(l => l[0] === " " || l[0] === "\t" ? l.slice(m) : l).join("\n");
+  }
+  return result
   // dedent eats leading and trailing whitespace too
-  result = result.trim();
-
+  .trim()
   // handle escaped newlines at the end to ensure they don't get stripped too
-  return result.replace(/\\n/g, "\n");
+  .replace(/\\n/g, "\n");
 }
-
-if (true) {
-  module.exports = dedent;
-}
+module.exports = exports.default;
+module.exports["default"] = exports.default;
 
 
 /***/ }),
