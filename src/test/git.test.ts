b0868a08ad6431bb0acdd43cc4912c883299a388
Nico Korthout <nicokorthout@gmail.com>
2023-08-08 19:08:11 +0200

    test: add git.test.ts
    Adds some tests to make sure git works as I expect

---
+++ src/test/git.test.ts
@@ -0,0 +1,35 @@
+import { Git, GitRefNotFoundError } from "../git";
+import { execa } from "execa";
+
+const git = new Git(execa);
+let response = { exitCode: 0, stdout: "" };
+
+jest.mock("execa", () => ({
+  execa: jest.fn(() => response),
+}));
+
+describe("git.fetch", () => {
+  describe("throws GitRefNotFoundError", () => {
+    it("when fetching an unknown ref, i.e. exit code 128", async () => {
+      response.exitCode = 128;
+      expect.assertions(3);
+      await git.fetch("unknown", "", 1).catch((error) => {
+        expect(error).toBeInstanceOf(GitRefNotFoundError);
+        expect(error).toHaveProperty(
+          "message",
+          "Expected to fetch 'unknown', but couldn't find it",
+        );
+        expect(error).toHaveProperty("ref", "unknown");
+      });
+    });
+  });
+
+  describe("throws Error", () => {
+    it("when failing with an unexpected non-zero exit code", async () => {
+      response.exitCode = 1;
+      await expect(git.fetch("unknown", "", 1)).rejects.toThrowError(
+        `'git fetch origin unknown' failed with exit code 1`,
+      );
+    });
+  });
+});
