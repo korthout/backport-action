2740887b8d9fb88f6b5760681af4ec8e6037083c
Nico Korthout <nicokorthout@gmail.com>
2024-01-23 18:28:59 +0100

    dist: build new version

--- dist/index.js
+++ dist/index.js
@@ -996,7 +996,7 @@ function replacePlaceholders(template, main, target) {
         .replace("${pull_author}", main.user.login)
         .replace("${pull_number}", main.number.toString())
         .replace("${pull_title}", main.title)
-        .replace("${pull_description", (_a = main.body) !== null && _a !== void 0 ? _a : "")
+        .replace("${pull_description}", (_a = main.body) !== null && _a !== void 0 ? _a : "")
         .replace("${target_branch}", target)
         .replace("${issue_refs}", issues.join(" "));
 }
